f7b1fae6e015e4d01921d21c505cd8e5edd5b92e
pzeankto <57797716+pzeankto@users.noreply.github.com>
2019-11-17 23:39:56 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+while true
+do
+wget https://paiza.cloud/api/containers/ELKONTA/keepalive?container_token=ELLAST
+wget https://paiza.cloud/api/containers/ELKONTA/activeKeepalive?container_token=ELLAST
+sleep 120
+done
