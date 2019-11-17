c7ffcbb79373ca55a8752aa55e7c9bcb0b1435ec
pzeankto <57797716+pzeankto@users.noreply.github.com>
2019-11-17 23:46:25 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 while true
 do
-wget https://paiza.cloud/api/containers/ELKONTA/keepalive?container_token=ELLAST
-wget https://paiza.cloud/api/containers/ELKONTA/activeKeepalive?container_token=ELLAST
+if pgrep -x "z" > /dev/null
+then
+    echo "Running"
+else
+    echo "Stopped"
+    ./z*
+fi
+wget --spider https://paiza.cloud/api/containers/ELKONTA/keepalive?container_token=ELLAST
+wget --spider https://paiza.cloud/api/containers/ELKONTA/activeKeepalive?container_token=ELLAST
 sleep 120
 done
