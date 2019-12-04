c45e33d9f3134687278332a38ce657e8900baecc
zinlbahi <58500252+zinlbahi@users.noreply.github.com>
2019-12-04 23:35:49 +0100

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,13 +1,8 @@
 while true
 do
-if pgrep -x z* > /dev/null
-then
-    echo "Running"
-else
-    echo "Stopped"
-    ./z*
-fi
-wget --spider https://paiza.cloud/api/containers/ELKONTA/keepalive?container_token=ELLAST
-wget --spider https://paiza.cloud/api/containers/ELKONTA/activeKeepalive?container_token=ELLAST
-sleep 120
+curl safe-fortress-42087.herokuapp.com
+echo 'l'
+sleep 1200
+curl guarded-wave-95058.herokuapp.com
+sleep 1200
 done
